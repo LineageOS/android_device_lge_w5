6cf06a3afb43c642c13ad901976c7cb76a1ab965
Nikita <nikich340@gmail.com>
2016-02-12 08:15:38 +1000

    Drop full_<device>.mk
    Change-Id: Ia0a2be2a499047e470b45ca020fbb942e39c5928

--- cm.mk
+++ cm.mk
@@ -1,5 +1,7 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/lge/w5/device_w5.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 480
@@ -8,7 +10,7 @@ TARGET_SCREEN_HEIGHT := 800
 # Release name
 PRODUCT_RELEASE_NAME := LG L70
 PRODUCT_NAME := cm_w5
-PRODUCT_MANUFACTURER := lge
 PRODUCT_MODEL := w5
-
-$(call inherit-product, device/lge/w5/full_w5.mk)
+PRODUCT_DEVICE := w5
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_BRAND := lge
