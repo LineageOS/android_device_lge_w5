a7e6653257f17018f7c4db419549956f8759b25f
Demon000 <demonsingur@gmail.com>
2017-07-02 18:16:33 +0300

    w5: Allow vendor init to override props
    Change-Id: I475703b19415c8aabf92c9c0ce5903981110767c

--- init/init_w5.cpp
+++ init/init_w5.cpp
@@ -28,6 +28,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -35,6 +37,17 @@
 #include "util.h"
 
 #define CHUNK 2048 /* read 2048 bytes at a time */
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 int check_cmdline(const char param[]) {
 
